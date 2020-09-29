1d6531f9968429c3aad050e16d351d9677bdb0b6
anuppatange <71494105+anuppatange@users.noreply.github.com>
2020-09-29 18:43:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ function setup() {
 
   car=createSprite(50,200,50,50);
   car.velocityX=speed;
+  car.shapeColor=color("red");
   wall=createSprite(1500,200,60,height/2);
   wall.shapeColor=color(80,80,80);
 }
